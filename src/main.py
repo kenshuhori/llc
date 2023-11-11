c5e84d4a449e96dcb31f7e7056776bbb4a3bd0ae
Kenshu Hori <kenshuhori@gmail.com>
2023-11-11 23:51:36 +0900

    devContainerをcompose.ymlベースで起動する

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ st.balloons()
 
 st.snow()
 
-df = pd.read_csv('llc/assets/csvs/llc.csv')
+df = pd.read_csv('assets/csvs/llc.csv')
 st.line_chart(data=df, x="年齢")
 
 # 誕生〜幼稚園
@@ -24,7 +24,7 @@ baby_col1, baby_col2 = st.columns(2)
 with baby_col1:
     st.subheader('小学校入学まで')
     st.text('幼稚園では、毎日泥団子を作って、誰が一番綺麗なまんまるで、硬くて表面はサラサラで、美しいかを競っていた気がします。')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with baby_col2:
@@ -36,7 +36,7 @@ elementary_col1, elementary_col2 = st.columns(2)
 with elementary_col1:
     st.subheader('小学校')
     st.text('頭悪かったです。でももしかしたら4/1生まれのせいかも。')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with elementary_col2:
@@ -48,7 +48,7 @@ junior_col1, junior_col2 = st.columns(2)
 with junior_col1:
     st.subheader('中学校')
     st.text('学校より塾が楽しかった。')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with junior_col2:
@@ -60,7 +60,7 @@ highschool_col1, highschool_col2 = st.columns(2)
 with highschool_col1:
     st.subheader('高校')
     st.text('記憶ないです。')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with highschool_col2:
@@ -72,7 +72,7 @@ academy_col1, academy_col2 = st.columns(2)
 with academy_col1:
     st.subheader('大学')
     st.text('1年目:楽しかった。2年目:単位とれねえ...3年目:もう勉強フル単。4年目:就活つらい')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with academy_col2:
@@ -84,7 +84,7 @@ worker_col1, worker_col2 = st.columns(2)
 with worker_col1:
     st.subheader('社会人')
     st.text('良い年と悪い年が交互にくる。')
-    image = Image.open('llc/assets/images/kuchipatchi_front.png')
+    image = Image.open('assets/images/kuchipatchi_front.png')
     st.image(image, width=200)
     
 with worker_col2:
